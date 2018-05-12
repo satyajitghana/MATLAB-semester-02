f9d3b889c1c38afca2b9332f5459f6e894dbf4db
Satyajit Ghana <satyajitghana7@gmail.com>
2018-05-12 17:58:21 +0530

    teeny-tiny mistake :P

--- trapezoid.m
+++ trapezoid.m
@@ -8,7 +8,7 @@ if numel(f) > 1
     I = (h/2)*(f(1) + f(end) + 2*sum(f(2:1:end-1)));
 else
     x = a:h:b;
-    I = (h/3)*(f(x(1)) + f(x(end)) + 2*sum(f(x(2:1:end-1))) );
+    I = (h/2)*(f(x(1)) + f(x(end)) + 2*sum(f(x(2:1:end-1))) );
 end
 
 % display the integral
